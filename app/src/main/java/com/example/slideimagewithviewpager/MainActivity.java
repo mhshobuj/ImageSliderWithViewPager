23addece11feec170e15b8e24a716526aa9658c9
DMA_Mehedi <dma.bd2018@gmail.com>
2023-02-26 11:24:22 +0600

    dot indicator image add

--- app/src/main/java/com/example/slideimagewithviewpager/MainActivity.java
+++ app/src/main/java/com/example/slideimagewithviewpager/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.viewpager.widget.ViewPager;
 import android.net.Uri;
 import android.os.Bundle;
 
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     ArrayList<String> images = new ArrayList<>();
     ViewPagerAdapter adapter;
+    WormDotsIndicator wormDotsIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = findViewById(R.id.viewPager);
+        wormDotsIndicator = (WormDotsIndicator) findViewById(R.id.worm_dots_indicator);
 
         images.add("https://images.pexels.com/photos/6898857/pexels-photo-6898857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
         images.add("https://images.pexels.com/photos/6897769/pexels-photo-6897769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
@@ -31,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ViewPagerAdapter(this, images);
         viewPager.setPadding(100, 0, 100, 0);
         viewPager.setAdapter(adapter);
+
+        wormDotsIndicator.attachTo(viewPager);
 
     }
 }
